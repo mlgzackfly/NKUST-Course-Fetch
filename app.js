ce296e5b3a067db1094239e1401fd5d71051cbbd
mlgzackfly <mlgzackfly@gmail.com>
2022-12-05 13:40:47 +0800

    chore: store file as json

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ async function fetchList() {
                 console.log(`${yms_value} : ${yms_name}`);
 
                 const cmp_area_id = $('select[id="cmp_area_id"] > option');
-                for (var cmp = 1; cmp < cmp_area_id.length; cmp++) {
+                for (var cmp = 2; cmp < cmp_area_id.length; cmp++) {
                     var cmp_value = cmp_area_id.eq(cmp).attr('value')
                     var cmp_area = cmp_area_id.eq(cmp).text()
                     console.log(`${cmp_value} : ${cmp_area}`);
@@ -33,6 +33,7 @@ async function fetchList() {
                         var dgr_name = dgr_id.eq(dgr).text()
                         console.log(`${dgr_value} : ${dgr_name}`);
                         fetchUntiId(yms_value, cmp_value, dgr_value)
+                        await sleep(1000)
                     }
                 }
             }
@@ -55,25 +56,25 @@ function fetchUntiId(yms_yms,cmp_area_id,dgr_id) {
                     'dgr_id':dgr_id,
                 },
 
-    },function (err, res,body) {
-        try {
-            // console.log("取得 UnitID :")
-            const $ = cheerio.load(body);
-            const unt_id = $('select[id="unt_id"] > option');
-            for (let unt = 1; unt < unt_id.length; unt++) {
-                let unt_value = unt_id.eq(unt).attr('value');
-                let unt_name = unt_id.eq(unt).text()
-                // console.log(`yms_yms : ${yms_yms}, cmp : ${cmp_area_id}, dgr : ${dgr_id} , ${unt_value} : ${unt_name}`)
+    },async function (err, res, body) {
+            try {
+                // console.log("取得 UnitID :")
+                const $ = cheerio.load(body);
+                const unt_id = $('select[id="unt_id"] > option');
+                for (let unt = 1; unt < unt_id.length; unt++) {
+                    let unt_value = unt_id.eq(unt).attr('value');
+                    let unt_name = unt_id.eq(unt).text()
+                    // console.log(`yms_yms : ${yms_yms}, cmp : ${cmp_area_id}, dgr : ${dgr_id} , ${unt_value} : ${unt_name}`)
 
-                fetchCourse(yms_yms,cmp_area_id,dgr_id,unt_value)
-                // console.log(`${unt_value} : ${unt_name}`);
+                    await fetchCourse(yms_yms, cmp_area_id, dgr_id, unt_value)
+                    await sleep(1000)
+                    // console.log(`${unt_value} : ${unt_name}`);
+                }
+                // jsonfile.writeFileSync(`./dist/${yms_yms}.json`, writeJson);
+            } catch (e) {
+                // console.log(e);
             }
-            // jsonfile.writeFileSync(`./dist/${yms_yms}.json`, writeJson);
         }
-        catch (e) {
-            // console.log(e);
-        }
-    }
     )
 }
 
@@ -99,38 +100,38 @@ async function fetchCourse(yms_yms,cmp_area_id,dgr_id,unt_value) {
            const $ = cheerio.load(body);
            const form = $('[name=thisform] tbody tr')
            for (let i =2; i< form.length; i++){
-               // for (let j =0; j < form.eq(i).children("td").length; j++){
-                   console.log("code : " + form.eq(i).children("td").eq(0).html().replace("&nbsp;", ""))
-                   console.log("cmp : " + form.eq(i).children("td").eq(1).html().replace("&nbsp;", ""))
-                   console.log("dgr : " + form.eq(i).children("td").eq(2).html().replace("&nbsp;", ""))
-                   console.log("unt : " + form.eq(i).children("td").eq(3).html().replace("&nbsp;", ""))
-                   console.log("class : " + form.eq(i).children("td").eq(4).html().replace("&nbsp;", ""))
-                   console.log("foreverID : " + form.eq(i).children("td").eq(6).html().replace("&nbsp;", ""))
-                   console.log("name : " + form.eq(i).children("td").eq(7).html().replace("&nbsp;", ""))
-                   console.log("hours : " + form.eq(i).children("td").eq(8).html().replace("&nbsp;", ""))
-                   console.log("units : " + form.eq(i).children("td").eq(9).html().replace("&nbsp;", ""))
-                   console.log("required : " + form.eq(i).children("td").eq(11).html().replace("&nbsp;", ""))
-                   console.log("instructors : " + form.eq(i).children("td").eq(12).html().replace("&nbsp;", ""))
-                   console.log("location : " + form.eq(i).children("td").eq(13).html().replace("&nbsp;", ""))
-                   console.log("people : " + form.eq(i).children("td").eq(14).html().replace("&nbsp;", ""))
-                   console.log("maxpeople : " + form.eq(i).children("td").eq(15).html().replace("&nbsp;", ""))
-                   console.log("time : " + form.eq(i).children("td").eq(16).html().replace("&nbsp;", ""))
-                   console.log("english : " + form.eq(i).children("td").eq(17).html().replace("&nbsp;", ""))
-                   console.log("remote : " + form.eq(i).children("td").eq(18).html().replace("&nbsp;", ""))
-                   console.log("href : " + form.eq(i).children("td").eq(19).attr("onclick").substring(9,form.eq(i).children("td").eq(19).attr("onclick").length - 2))
-                   console.log("note : " + form.eq(i).children("td").eq(20).html().replace("&nbsp;", ""))
-                   console.log("====================")
-               // if (j !== form.eq(i).children("td").length -2 )
-                   // {
-                   //     console.log(form.eq(i).children("td").eq(j).html().replace("&nbsp;", ""))
-                   // }
-               // }
-
+               console.log("====================")
+               let code = form.eq(i).children("td").eq(0).html().replace("&nbsp;", "")
+               let cmp =  form.eq(i).children("td").eq(1).html().replace("&nbsp;", "")
+               let dgr =  form.eq(i).children("td").eq(2).html().replace("&nbsp;", "")
+               let unt = form.eq(i).children("td").eq(3).html().replace("&nbsp;", "")
+               let className = form.eq(i).children("td").eq(4).html().replace("&nbsp;", "")
+               let foreverID = form.eq(i).children("td").eq(6).html().replace("&nbsp;", "")
+               let courseName = form.eq(i).children("td").eq(7).html().replace("&nbsp;", "")
+               let hours = form.eq(i).children("td").eq(8).html().replace("&nbsp;", "")
+               let units = form.eq(i).children("td").eq(9).html().replace("&nbsp;", "")
+               let required = form.eq(i).children("td").eq(11).html().replace("&nbsp;", "")
+               let instructors = form.eq(i).children("td").eq(12).html().replace("&nbsp;", "")
+               let position = form.eq(i).children("td").eq(13).html().replace("&nbsp;", "")
+               let people = form.eq(i).children("td").eq(14).html().replace("&nbsp;", "")
+               let maxpeople = form.eq(i).children("td").eq(15).html().replace("&nbsp;", "")
+               let time = form.eq(i).children("td").eq(16).html().replace("&nbsp;", "")
+               let english = form.eq(i).children("td").eq(17).html().replace("&nbsp;", "")
+               let remote = form.eq(i).children("td").eq(18).html().replace("&nbsp;", "")
+               let href = form.eq(i).children("td").eq(19).attr("onclick").substring(9,form.eq(i).children("td").eq(19).attr("onclick").length - 2).split("','")
+               let note = form.eq(i).children("td").eq(20).html().replace("&nbsp;", "")
+               array.push(Object.assign({code,cmp,dgr,unt,className,foreverID,courseName,hours,units,required,instructors,position,people,maxpeople,time,english,remote,href,note}));
+               console.log(array)
            }
+           jsonfile.writeFileSync(`./dist/${yms_yms}/${unt_value}.json`, JSON.stringify(array));
        } catch (e) {
 
        }
     })
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function main() {
